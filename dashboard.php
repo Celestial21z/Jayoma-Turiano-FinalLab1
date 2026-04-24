bc5dedb7a6ccc98cf2a38a119bde52def6f7d22f
pepepo2 <philipjayoma2003@gmail.com>
2026-04-24 22:48:33 +0800

    added styles on dashboard.php and index.php

--- dashboard.php
+++ dashboard.php
@@ -11,25 +11,29 @@
     $lastVisit = isset($_COOKIE['last_visit']) ? $_COOKIE['last_visit'] : "First visit";
 
     date_default_timezone_set("Asia/Manila");
-    $currentDateTime = date("F j, Y, - g:i a");
+    $currentDateTime = date("F j, Y, - g:i A");
 
     setcookie("last_visit", $currentDateTime, time() + (86400), "/");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 </head>
 <body>
-    <h1>Welcome, <?php echo $name; ?>!</h1>
+    <div class="container mt-5">
+        <h1>Welcome, <?php echo $name; ?>!</h1>
 
     <p>Current date and time: <?php echo $currentDateTime; ?></p>
 
-    <p>Last visit: <?php echo $lastVisit; ?></p>
-    
-    
-    <a href="logout.php">Logout</a>
+        <p>Last visit: <?php echo $lastVisit; ?></p>
+
+
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+
 <?php
     session_start();
-
+    
     if (isset($_SESSION['name'])) {
         header("Location: dashboard.php");
         exit();
@@ -9,6 +10,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,11 +19,13 @@
 </head>
 
 <body>
-    <form method="POST" action="">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-        <button type="submit">Submit</button>
-    </form>
+    <div class="container mt-5">
+        <form method="POST" action="">
+            <label for="name">Enter Name:</label>
+            <input type="text" maxlength="30" id="name" name="name" required>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
     
     <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
